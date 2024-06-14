94815b31ed082ca6b603190f04a1b4233874add1
scotthowson <scott_howson@corruptiongames.net>
2024-06-14 15:04:50 -0400

    Build-Tools

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -xe
+
+ADAPTATION_TOOLS_BRANCH=main
+if [ ! -d build ]; then
+  git clone -b $ADAPTATION_TOOLS_BRANCH https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools build
+fi
+
+./build/build.sh "$@"
